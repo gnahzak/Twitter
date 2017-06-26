4f4646698bc9ed32239c0386f9322749858002a9
gnahzak <kazhang@fb.com>
2017-06-26 13:16:35 -0700

    Changed file names

--- app/src/main/java/com/codepath/apps/twitterApp/TwitterApplication.java
+++ app/src/main/java/com/codepath/apps/twitterApp/TwitterApplication.java
@@ -12,7 +12,7 @@ import com.raizlabs.android.dbflow.config.FlowManager;
  * including the image cache in memory and on disk. This also adds a singleton
  * for accessing the relevant rest client.
  *
- *     RestClient client = RestApplication.getRestClient();
+ *     TwitterClient client = TwitterApplication.getRestClient();
  *     // use client to send requests to API
  *
  */
@@ -29,7 +29,7 @@ public class TwitterApplication extends Application {
 		TwitterApplication.context = this;
 	}
 
-	public static RestClient getRestClient() {
-		return (RestClient) RestClient.getInstance(RestClient.class, TwitterApplication.context);
+	public static TwitterClient getRestClient() {
+		return (TwitterClient) TwitterClient.getInstance(TwitterClient.class, TwitterApplication.context);
 	}
 }
